7d095d97929494c67917d411c8e2cdaf20852418
mentalilll <159670055+mentalilll@users.noreply.github.com>
2024-02-15 23:58:40 +0100

    Update vpd-chart-card.js
    Added Mouseover tooltip with configuration option,
    Optimized Table and Rendering,
    Optimized Performance and save data matrix into localStorage

--- vpd-chart-card.js
+++ vpd-chart-card.js
@@ -8,10 +8,12 @@ class VpdTableCard extends HTMLElement {
             max_humidity: {type: Number},
             steps_temperature: {type: Number},
             steps_humidity: {type: Number},
-            vpd_phases: {type: Array}
+            vpd_phases: {type: Array},
+            air_text: {type: String},
+            rh_text: {type: String},
+            enable_tooltip: {type: Boolean}
         };
     }
-
     constructor() {
         super();
         this.vpd_phases = [
@@ -21,13 +23,17 @@ class VpdTableCard extends HTMLElement {
             {lower: 1.2, upper: 1.6, className: 'mid-late-flower'},
             {lower: 1.6, className: 'danger-zone'},
         ];
+        this.vpdCache = new Map();
         this.sensors = [];
         this.min_temperature = 5;
-        this.max_temperature = 40;
+        this.max_temperature = 35;
         this.min_humidity = 10;
         this.max_humidity = 90;
         this.steps_temperature = .5;
         this.steps_humidity = 1;
+        this.enable_tooltip = true;
+        this.airText = "Air";
+        this.rhText = "RH";
     }
 
     // Whenever the state changes, a new `hass` object is set. Use this to
@@ -37,97 +43,164 @@ class VpdTableCard extends HTMLElement {
         if (!this.content) {
             this.innerHTML = `
         <ha-card>
-        <style>
-            ha-card, ha-card .vpd-card-container {
-                height:100%;
-                min-height:400px;
-            }
-            body,html {
-                font-family: "Segoe UI", Arial, sans-serif;
-                font-size: 10px;
-                margin:0;
-                padding:0;
-                height:100%;
-                position:relative;
-            }
-            .danger-zone { background-color: #ce4234; }
-            .early-veg { background-color: #22ab9c; }
-            .late-veg { background-color: #9cc55b; }
-            .mid-late-flower { background-color: #e7c12b; }
-            .under-transpiration { background-color: #1a6c9c; }
-            table, th, td { border: 1px solid rgba(255,255,255,0.05); border-collapse: collapse; }
-            th, td { text-align: center; min-width:2px;}
-            .highlight {
-                background-color: white !important; /* oder eine andere Farbe Ihrer Wahl */
-                color:#333333 !important;
-            }
-            table {
-                width:100%;
-                height:100%;
-            }
-            .highlight {
-                position: relative;
-                z-index: 2;
-            }
-            .custom-tooltip {
-                width: 240px;
-                background-color: rgba(0, 0, 0, 0.5);
-                color: #fff;
-                text-align: left;
-                border-radius: 6px;
-                padding: 5px;
-                position: absolute;
-                z-index: 3;
-                bottom: 100%;
-                left: 50%;
-                margin-left: 10px; 
-                transition: opacity 0.3s;
-                font-size: 12px; 
-            }
-           
-            .custom-tooltip:focus {
-                z-index:9999999;
-            }
-
-            .highlight:hover .custom-tooltip {
-                visibility: visible;
-                opacity: 1;
-            }
-
-            .highlight {
-                cursor: pointer; 
-            }
-            .vpd-table {
-                display: table;
-                width: 100%;
-                height: 100%;
-            }
-            .row {
-                display: table-row;
-                background-color: #fff;
-            }
-            .cell {
-               display: table-cell;
-            }
+            <style>
+                ha-card, ha-card .vpd-card-container {
+                    height: 100%;
+                    min-height: 400px;
+                    overflow:hidden;
+                }
+                
+                body, html {
+                    font-family: "Segoe UI", Arial, sans-serif;
+                    font-size: 10px;
+                    margin: 0;
+                    padding: 0;
+                    height: 100%;
+                    position: relative;
+                }
+                
+                .danger-zone {
+                    background-color: #ce4234;
+                }
+                
+                .early-veg {
+                    background-color: #22ab9c;
+                }
+                
+                .late-veg {
+                    background-color: #9cc55b;
+                }
+                
+                .mid-late-flower {
+                    background-color: #e7c12b;
+                }
+                
+                .under-transpiration {
+                    background-color: #1a6c9c;
+                }
+                
+                table {
+                    width: 100%;
+                    height: 100%;
+                }
+                
+                table, th, td {
+                    border: 1px solid rgba(255, 255, 255, 0.05);
+                    border-collapse: collapse;
+                }
+                
+                th, td {
+                    text-align: center;
+                    min-width: 2px;
+                }
+                
+                .highlight {
+                    position: relative;
+                    z-index: 2;
+                    background-color: white !important; /* oder eine andere Farbe Ihrer Wahl */
+                    color: #333333 !important;
+                    cursor: pointer;
+                }
+                .mousePointer {
+                    z-index:9999;
+                }
+                .custom-tooltip {
+                    bottom: 150%;
+                    
+                    margin-bottom: 5px;
+                    /*margin-left: -120px;*/
+                    margin-left: -5px;
+                    padding: 7px;
+                    width: 240px;
+                    -webkit-border-radius: 3px;
+                    -moz-border-radius: 3px;
+                    border-radius: 3px;
+                    background-color: #000000;
+                    background-color: hsla(0, 0%, 20%, 0.9);
+                    color: #ffffff;
+               
+                    text-align: center;
+                    font-size: 12px;
+                    line-height: 1.2;
+                    position: absolute;
+                }
+                
+                .custom-tooltip:after {
+                
+                    position: absolute;
+                    bottom: -15%;
+                    left: 2%;
+                    
+                    width: 0;
+                    border-top: 5px solid #000000;
+                    border-top: 5px solid hsla(0, 0%, 20%, 0.9);
+                    border-right: 5px solid transparent;
+                    border-left: 5px solid transparent;
+                    content: " ";
+                    font-size: 0;
+                    line-height: 0;
+                }
+                
+                .vpd-table {
+                    display: table;
+                    width: 100%;
+                    height: 100%;
+                }
+                
+                .row {
+                    display: table-row;
+                    background-color: #ffffff;
+                }
+                
+                .cell {
+                    display: table-cell;
+                }
             </style>
             <div class="vpd-card-container"></div>
+            <div class="highlight mousePointer" style="opacity:0">
+                <div class="custom-tooltip"></div>
+            </div> <!-- Tooltip -->
         </ha-card>
       `;
             this.content = this.querySelector("div.vpd-card-container");
         }
-        this.content.innerHTML = "";
-        const rows = Math.ceil((this.max_temperature - this.min_temperature) / this.steps_temperature);
-        const columns = Math.ceil((this.max_humidity - this.min_humidity) / this.steps_humidity);
-        // Setze die berechneten Werte als CSS-Variablen
-        this.style.setProperty('--rows', rows);
-        this.style.setProperty('--columns', columns);
-        this.initTable(hass);
-
-
+        let table;
+        if (localStorage.getItem('vpd-table-card')) {
+            table = localStorage.getItem('vpd-table-card');
+        } else {
+            table = this.buildTable();
+            localStorage.setItem('vpd-table-card', table);
+        }
+        this.content.innerHTML = "<div class='vpd-table'>" + table + "</div>";
+        this.buildTooltip(this.content, hass);
+        if (this.enable_tooltip) {
+            this.content.addEventListener('mouseover', (event) => {
+                if (event.target.classList.contains('cell')) {
+                    this.buildMouseTooltip(event.target, hass);
+                }
+            });
+            this.addEventListener('mouseleave', (event) => {
+                let tooltip = this.querySelector('.mousePointer');
+                let fadeOut = setInterval(function () {
+                    if (!tooltip.style.opacity) {
+                        tooltip.style.opacity = 1;
+                    }
+                    if (tooltip.style.opacity > 0) {
+                        tooltip.style.opacity -= 0.1;
+                    } else {
+                        clearInterval(fadeOut);
+                    }
+                }, 100);
+            });
+        }  else {
+            this.querySelector('.mousePointer').style.display = 'none';
+        }
     }
-
     setConfig(config) {
         this.config = config;
+        if (!config.sensors) {
+            throw new Error('You need to define sensors');
+        }
 
         this.vpd_phases = config.vpd_phases || this.vpd_phases;
         this.sensors = config.sensors || this.sensors;
@@ -143,33 +216,6 @@ class VpdTableCard extends HTMLElement {
 
     getCardSize() {
         return 3;
-    }
-    initTable(hass) {
-        const container = this.content;
-        container.innerHTML = '';
-
-        const table = document.createElement('div');
-        table.className = 'vpd-table';
-        container.appendChild(table);
-        this.buildTable(table);
-        this.buildTooltip(table, hass);
-    }
-
-    buildTable(tbody) {
-        for (let Tair = this.min_temperature; Tair <= this.max_temperature; Tair += this.steps_temperature) {
-            const Tleaf = Tair - 2;
-            const row = document.createElement('div');
-            row.className = 'row';
-            tbody.appendChild(row);
-
-            for (let RH = this.max_humidity; RH >= this.min_humidity; RH -= this.steps_humidity) {
-                const vpd = this.calculateVPD(Tleaf, Tair, RH);
-                const phaseClass = this.getPhaseClass(vpd);
-                const cell = document.createElement('div');
-                row.appendChild(cell);
-                cell.className = `${phaseClass} cell`;
-            }
-        }
     }
 
     calculateVPD(Tleaf, Tair, RH) {
@@ -191,12 +237,56 @@ class VpdTableCard extends HTMLElement {
         return '';
     }
 
+    buildTable() {
+        const table = document.createElement('div');
+        table.className = 'vpd-table';
+
+        requestAnimationFrame(() => {
+            const fragment = document.createDocumentFragment();
+            let html = '';
+            for (let Tair = this.min_temperature; Tair <= this.max_temperature; Tair += this.steps_temperature) {
+                const Tleaf = Tair - 2;
+                html += '<div class="row">';
+
+                for (let RH = this.max_humidity; RH >= this.min_humidity; RH -= this.steps_humidity) {
+                    const key = `${Tleaf}-${Tair}-${RH}`;
+                    let vpd;
+                    if (this.vpdCache.has(key)) {
+                        vpd = this.vpdCache.get(key);
+                    } else {
+                        vpd = this.calculateVPD(Tleaf, Tair, RH).toFixed(2);
+                        this.vpdCache.set(key, vpd);
+                    }
+                    const phaseClass = this.getPhaseClass(vpd);
+                    html += `<div class="cell ${phaseClass}" data-air="${Tair}" data-leaf="${Tleaf}" data-rh="${RH}" data-vpd="${vpd}"></div>`;
+                }
+
+                html += '</div>';
+            }
+            const tempDiv = document.createElement('div');
+            localStorage.setItem('vpd-table-card', html);
+            tempDiv.innerHTML = html;
+            while (tempDiv.firstChild) {
+                fragment.appendChild(tempDiv.firstChild);
+            }
+
+            table.appendChild(fragment);
+        });
+
+        return table;
+    }
+
+
     buildTooltip(table, hass) {
-        this.config.sensors.forEach((sensor, index)=> {
+        const fragment = document.createDocumentFragment();
+        this.config.sensors.forEach((sensor, index) => {
             let humidity = hass.states[sensor.humidity].state;
             let temperature = hass.states[sensor.temperature].state;
-            let leafTemperature = hass.states[sensor.temperature].state || (temperature - 2);
-            let name = sensor.name || `Tent ${index + 1}`;
+            let leafTemperature = temperature - 2;
+            if (sensor.leaf_temperature !== undefined) {
+                leafTemperature = hass.states[sensor.leaf_temperature].state;
+            }
+            let vpd = sensor.vpd || this.calculateVPD(parseFloat(leafTemperature), parseFloat(temperature), parseFloat(humidity)).toFixed(2)
             const relativeHumidity = this.max_humidity - humidity; // Umkehren der Berechnung
             const totalHumidityRange = this.max_humidity - this.min_humidity;
             const percentageHumidity = (relativeHumidity / totalHumidityRange) * 100;
@@ -236,15 +326,42 @@ class VpdTableCard extends HTMLElement {
             const tooltip = document.createElement('div');
             tooltip.className = 'custom-tooltip';
 
-            let vpd =  sensor.vpd || this.calculateVPD(parseFloat(leafTemperature), parseFloat(temperature), parseFloat(humidity)).toFixed(2);
-
-            tooltip.innerHTML = `<strong>${name}:</strong> kPa: ${vpd} | ${this.rhText}: ${humidity}% | ${this.airText}: ${temperature}°C`;
-
+            tooltip.innerHTML = `<strong>${sensor.name}:</strong> kPa: ${vpd} | ${this.rhText}: ${humidity}% | ${this.airText}: ${temperature}°C`;
             circle.appendChild(tooltip);
-            table.appendChild(circle);
-        })
 
+            fragment.appendChild(circle);
+        });
 
+        requestAnimationFrame(() => {
+            table.appendChild(fragment);
+        });
+    }
+
+    buildMouseTooltip(target) {
+        const humidity = target.getAttribute('data-rh');
+        const temperature = target.getAttribute('data-air');
+        const vpd = parseFloat(target.getAttribute('data-vpd')).toFixed(2);
+        const percentageHumidity = ((this.max_humidity - humidity) / (this.max_humidity - this.min_humidity)) * 100;
+        const percentageTemperature = ((temperature - this.min_temperature) / (this.max_temperature - this.min_temperature)) * 100;
+
+        let circle = this.querySelector('.mousePointer');
+        circle.className = 'highlight mousePointer';
+        circle.style.cssText = `width: 10px; height: 10px; background-color: white; border-radius: 50%; position: absolute; transform: translateX(-50%); left: ${percentageHumidity}%; bottom: ${100 - percentageTemperature}%;`;
+        let tooltip = circle.querySelector('.custom-tooltip') || document.createElement('div');
+        tooltip.className = 'custom-tooltip';
+        tooltip.innerHTML = `<strong>kPa:</strong> ${vpd} | <strong>${this.rhText}:</strong> ${humidity}% | <strong>${this.airText}:</strong> ${temperature}°C`;
+        if (!this.content.querySelector('.mousePointer')) {
+            circle.appendChild(tooltip);
+        }
     }
 }
+
 customElements.define('vpd-table-card', VpdTableCard);
+window.customCards = window.customCards || [];
+window.customCards.push({
+    type: "vpd-table-card",
+    name: "VPD Table Card",
+    preview: false, // Optional - defaults to false
+    description: "A custom card to display VPD values in a table",
+    documentationURL: "https://github.com/mentalilll/ha-vpd-chart", // Adds a help link in the frontend card editor
+});
